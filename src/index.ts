d658d8255f54b4e03669e21f724fa63fb66ba4dd
Francisco Figueiredo <fran.fig@gmail.com>
2019-05-30 10:42:24 -0300

    Chico: improve help

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import args from 'args';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -8,13 +10,24 @@ args
     .option(['i','input','input-folder','input-folder-yaml'], 'Folder when are the yaml open3 api description','api/')
     .option(['o','output','output-folder','output-folder-models'], 'Folder when write the mogoose models. the program backup older versions for default','models/')
     .option(['n','no-backup'], 'Don\'t backup older versions of moongose models')
-    .option(['h','?','help'], 'Show this help',false);
+    .option(['h','?','help'], 'Show this help',false)
+    .examples([
+        { usage: 'openapi3-to-mongose-typescript -h', description: 'show this helps' },
+        { usage: 'openapi3-to-mongose-typescript -i server/api', description: 'read yaml files with OpenAPI 3 Specification in folder "server/api/" and generate typescript files in "models/" on current directory' },
+        { usage: 'openapi3-to-mongose-typescript -o database/models', description: 'read yaml files with Swagger Specification in folder "api/" and generate typescript files in "database/models/" on current directory' },
+        { usage: 'openapi3-to-mongose-typescript -i docs/api -o db/models', description: 'read yaml files with OpenAPI 3 Specification in folder "docs/api/" and generate typescript files in "db/models/" on current directory' },
+    ]);
 
 
-const config = args.parse(process.argv);
+const config = args.parse(process.argv,{
+    name: 'openapi3-to-mongose-typescript',
+    version: true,
+    value: ''
+});
 
 if(config.help) {
-    args.showHelp()
+    args.showHelp();
+    args.showVersion();
 }
 
 const apiFolder = path.resolve(process.cwd() + '/' + config.input);
